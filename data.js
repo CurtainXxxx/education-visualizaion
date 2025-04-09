5a948b4c64681f6efb9cefb864644deef07e7166
xiaojian fei <18617346516@163.com>
2025-04-09 23:21:39 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,24 @@
+// data.js (示例数据)
+window.educationData = {
+    map: [
+        {"name": "广东", "value": 82.3, "teachers": 1256800, "funds": 5874.31},
+        {"name": "江苏", "value": 85.6, "teachers": 886700, "funds": 4006.79},
+        {"name": "浙江", "value": 84.2, "teachers": 684500, "funds": 3874.31},
+        {"name": "山东", "value": 79.8, "teachers": 987600, "funds": 3874.31},
+        {"name": "河南", "value": 72.5, "teachers": 923400, "funds": 2969.57},
+        {"name": "四川", "value": 75.3, "teachers": 856700, "funds": 3874.31},
+        {"name": "河北", "value": 71.4, "teachers": 856700, "funds": 2536.80},
+        {"name": "湖南", "value": 73.2, "teachers": 654300, "funds": 2536.80},
+        {"name": "湖北", "value": 76.1, "teachers": 625600, "funds": 2196.28},
+        {"name": "安徽", "value": 69.7, "teachers": 553200, "funds": 2196.28}
+    ],
+    bar: {
+        xAxis: ["广东", "江苏", "浙江", "山东", "河南", "四川", "河北", "湖南", "湖北", "安徽"],
+        series: [1256800, 886700, 684500, 987600, 923400, 856700, 856700, 654300, 625600, 553200]
+    },
+    pie: [
+        {"name": "基础教育", "value": 45},
+        {"name": "高等教育", "value": 35},
+        {"name": "职业教育", "value": 20}
+    ]
+};
